92038810c40b670359f42c5f8dc4c9513c2d57dc
OzgeBagci <egzobagci@gmail.com>
2024-07-28 15:44:03 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/OzgeBagci">Øzge Bagci</a> and is{" "}
@@ -16,7 +17,7 @@ export default function App() {
           >
             open-sourced on GitHub{" "}
           </a>
-          and
+          and{" "}
           <a href="https://weatherapp-reactproject.netlify.app/">
             hosted on Netlify
           </a>{" "}
