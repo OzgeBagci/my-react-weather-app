86645521f1ae7ad2454119699ea9e50bd443a958
OzgeBagci <egzobagci@gmail.com>
2024-08-24 00:00:16 +0200

    Corrected some errors

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-  function handleResponse(response) {}
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+  }
 
-  let apiKey = "1494f5f7ob4a4dtd10b403df6ba9a052";
-  let longitude = props.coordinate.lon;
-  let latitude = props.coordinate.lat;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+  function getForecast() {
+    let apiKey = "1494f5f7ob4a4dtd10b403df6ba9a052";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <div className="WeatherIcon">
-            <img
-              src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.data.condition.icon}.png`}
-              alt={props.data.condition.description}
-            />
+  if (forecast) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+            <div className="WeatherIcon">
+              <img
+                src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecast[0].condition.icon}.png`}
+                alt={forecast[0].condition.description}
+              />
+            </div>
+          </div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
           </div>
         </div>
-        <div className="WeatherForecast-temperatures">
-          <span className="WeatherForecast-temperature-max">19°</span>
-          <span className="WeatherForecast-temperature-min">10°</span>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    getForecast();
+    return "Loading...";
+  }
 }
