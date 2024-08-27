89359b808ae9a86e2d06e3f86784d4792486670f
OzgeBagci <egzobagci@gmail.com>
2024-08-27 21:58:37 +0200

    Refactored some code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -9,7 +9,7 @@ export default function WeatherForecast(props) {
 
   useEffect(() => {
     setLoaded(false);
-  }, [props.coordinates, props.unit]);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -20,8 +20,7 @@ export default function WeatherForecast(props) {
     let apiKey = "1494f5f7ob4a4dtd10b403df6ba9a052";
     let longitude = props.coordinates.longitude;
     let latitude = props.coordinates.latitude;
-    let unit = props.unit === "celsius";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units={unit}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
