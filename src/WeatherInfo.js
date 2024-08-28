4efeade063b304fae092c92943383586bb9dc7dd
OzgeBagci <egzobagci@gmail.com>
2024-08-28 12:22:23 +0200

    Refactored my code and changed css
    Refactored my code and got Fahrenheit displayed and changed the layout for the wind and humidity component.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,32 +1,35 @@
 import React from "react";
 import WeatherTemperature from "./WeatherTemperature";
 import FormattedDate from "./FormattedDate";
-import "./Weather.css";
 
 export default function WeatherInfo(props) {
   return (
-    <div>
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <span className="col-6">
-          <li>{props.data.description}</li>
-          <img src={props.data.icon_url} alt="forecast-icon" />
-        </span>
-        <WeatherTemperature
-          celsius={props.data.temperature}
-          unit={props.unit}
-          onUnitChange={props.onUnitChange}
-        />
-      </ul>
-      <span>
+    <div className="row align-items-center">
+      <div className="col-6">
+        <h1>{props.data.city}</h1>
         <ul>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.data.wind} km/h</li>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <span className="col-6">
+            <li>{props.data.description}</li>
+            <img src={props.data.icon_url} alt="forecast-icon" />
+          </span>
+          <WeatherTemperature
+            celsius={props.data.temperature}
+            unit={props.unit}
+            onUnitChange={props.onUnitChange}
+          />
         </ul>
-      </span>
+      </div>
+      <div className="col-6 justify-content-end">
+        <span>
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </span>
+      </div>
     </div>
   );
 }
